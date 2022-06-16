e400f1702f4c96793e65f6c70f677cf37203b106
olivinny <vinicius.gtoliv@gmail.com>
2022-06-16 15:16:36 -0300

    Exercicio pratico primeiros passos para desenvolvimento web

---
+++ Primeiros-passos-para-o-desenvolvimento-web/index2.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+  <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+    <title>Meu segundo site PHP! hahaha</title>
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+    <?php
+    for ($i=0;$i<10;$i++){
+      print("<span class=\"linha\">Linha número ".$i. "</span><br />");
+    }
+    ?>
+    <script src="./script.js"></script>
+  </body>
+</html>
---
+++ Primeiros-passos-para-o-desenvolvimento-web/script.js
@@ -0,0 +1,11 @@
+//alerta
+const linha = document.querySelector(".linha");
+if (linha) {
+  $(document).ready(function () {
+    alert("woohoo!");
+  });
+}
+//acordeon
+$(function () {
+  $("#acordeom").accordion();
+});
